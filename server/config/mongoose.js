974b6485aab4b16f321d0fa7c5e585d404abba57
jbeadles <jbeadles@xogrp.com>
2014-03-06 11:28:14 -0600

    Add signout functionality

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    crypto = require('crypto');
 
 module.exports = function(config,env) {
     mongoose.connect(config.db);
@@ -11,15 +12,39 @@ module.exports = function(config,env) {
     var userSchema = mongoose.Schema({
         firstName: String,
         lastName: String,
-        username: String
+        username: String,
+        salt: String,
+        hashed_pw: String
     });
+    userSchema.methods = {
+        authenticate: function(passwordToMatch) {
+            return hashPwd(this.salt, passwordToMatch) === this.hashed_pw;
+        }
+    };
+
     var User = mongoose.model('User', userSchema);
 
     User.find({}).exec(function(err, collection) {
         if(collection.length === 0) {
-            User.create({firstName: 'Joe', lastName: 'Eames', username: 'joe'});
-            User.create({firstName: 'John', lastName: 'Papa', username: 'john'});
-            User.create({firstName: 'Dan', lastName: 'Wahlin', username: 'dan'});
+            var salt, hash;
+            salt = createSalt();
+            hash = hashPwd(salt, 'joe');
+            User.create({firstName: 'Joe', lastName: 'Eames', username: 'joe', salt: salt, hashed_pw: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'john');
+            User.create({firstName: 'John', lastName: 'Papa', username: 'john', salt: salt, hashed_pw: hash});
+            salt = createSalt();
+            hash = hashPwd(salt, 'dan');
+            User.create({firstName: 'Dan', lastName: 'Wahlin', username: 'dan', salt: salt, hashed_pw: hash});
         }
     });
 };
+
+function createSalt() {
+    return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+    var hmac = crypto.createHmac('sha1', salt);
+    return hmac.update(pwd).digest('hex');
+}
