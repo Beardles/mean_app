2ee9de54aec2411e5171530025b37d0ecad2df98
Beardles <jbeardles@gmail.com>
2014-03-06 18:25:17 -0600

    Add admin module

--- public/app/account/mvIdentity.js
+++ public/app/account/mvIdentity.js
@@ -7,7 +7,10 @@ angular.module('app').factory('mvIdentity', function($window, mvUser) {
     return {
         currentUser: currentUser,
         isAuthenticated: function() {
-            return !!this.currentUser
+            return !!this.currentUser;
+        },
+        isAuthorized: function(role) {
+            return !!this.currentUser && this.currentUser.roles.indexOf(role) > -1;
         }
     }
 });
